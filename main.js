e144df332115c72ca41132ea310fe38289f594e7
rofl <88626207+roflmaybe@users.noreply.github.com>
2023-09-25 21:55:51 +0200

    0.0.2
    updated source with mongodb shit for uploading (soon to come)

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
+require('dotenv').config()
 const Koa = require('koa')
 const Router = require('koa-router')
 const serve = require('koa-static')
+const send = require('koa-send')
 const fs = require('fs')
 const path = require('path')
-
+const mongoose = require('mongoose')
+const koaBody = require('koa-body')
 
 const app = new Koa()
-const router = new Router
+const router = new Router()
 
 const imagedir = path.join(__dirname, 'images')
 
+mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const catSchema = new mongoose.Schema({
+    filename: String,
+})
+
+const Cat = mongoose.model('Cat', catSchema)
+
 app.use(serve(imagedir))
 app.use(router.routes())
-app.use(router.allowedMethods)
+app.use(router.allowedMethods())
+app.use(koaBody({ multipart: true }))
 
 router.get('/zumocat', (ctx) => {
     const imageFiles = fs.readdirSync(imagedir)
@@ -22,6 +34,8 @@ router.get('/zumocat', (ctx) => {
     ctx.type = path.extname(randomZumoCatImage)
     ctx.body = fs.createReadStream(path.join(imagedir, randomZumoCatImage))
 })
+
+
 
 app.listen(80, () => {
     console.log('[Server] - running on port 80')
